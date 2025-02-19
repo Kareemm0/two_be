bf2418e45a207b6287beb3b295bd3de3bb5016ea
kareemAfaq <kareemafaq53@gmail.com>
2025-02-19 17:16:45 +0200

    start_at_auth

--- lib/Features/Auth/presentation/view/login_screen.dart
+++ lib/Features/Auth/presentation/view/login_screen.dart
@@ -1,10 +1,31 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:two_be/core/utils/app_colors.dart';
+import 'package:two_be/core/utils/app_images.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(top: 24),
+        child: Column(
+          children: [
+            Expanded(child: Image.asset(AppImages.logo)),
+            Container(
+              padding: EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: AppColors.primaryColor,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(24),
+                  topRight: Radius.circular(24),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
