f3d05dbfe65f67bf42c8613cfa2563ea278dc752
kareem_m0hmed <km7404714@gmail.com>
2025-02-21 19:47:12 +0200

    search

--- lib/Features/search/presentation/views/search_screen.dart
+++ lib/Features/search/presentation/views/search_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:two_be/Features/categories/presentation/widget/custom_header_and_icon.dart';
+import 'package:two_be/core/utils/app_sizes.dart';
+import 'package:two_be/core/widgets/custom_search_bar.dart';
 
 class SearchScreen extends StatelessWidget {
   const SearchScreen({super.key});
@@ -6,7 +9,15 @@ class SearchScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text("Search Screen "),
-    );
+        body: Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 40),
+      child: Column(
+        children: [
+          CustomHeaderAndIcon(title: "بحث"),
+          height(30),
+          CustomSearchBar(controller: TextEditingController())
+        ],
+      ),
+    ));
   }
 }
